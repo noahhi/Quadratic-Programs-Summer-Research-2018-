2e420b41eca25c7b652b35a27ea33614d29e92f3
Hunt-Isaak <huntisan@dickinson.edu>
2018-06-18 13:42:52 -0400

    gurobi linearizations all work for knapsack. glover_ext not getting optimal int_gap for some reason

--- quadratics.py
+++ quadratics.py
@@ -5,26 +5,26 @@ class Quadratic: #base class for all quadratic problem types
 	def __init__(self, seed_=0, n_=10, m_=1, density_=100, symmetric_=False, k_item_=False):
 		"""
 		:param seed: The seed used for random number generation
-		:param n: The number of items in the knapsack 
+		:param n: The number of items in the knapsack
 		:param m: For multiple knapsack constraints
 		:param density: The density of non-zero value items in the knapsack, value between 1-100
 		:param symmetric: Determines if coefficient matrix should be symmetric or upper triangular
-		:param k_item: Turns into a k-item Quadratic Knapsack Problem 
+		:param k_item: Turns into a k-item Quadratic Knapsack Problem
 		"""
 		#declare/initialize common attributes
 		self.seed = seed_
 		np.random.seed(seed_)
 		self.n = n_
 		self.m = m_
-		self.density = density_	
+		self.density = density_
 		self.symmetric = symmetric_
-		self.k_item = k_item_ 	
+		self.k_item = k_item_
 		self.num_items = []
 		self.a = []
 		self.b = []
 		self.c = np.zeros(n_)
 		self.C = np.zeros((n_,n_))
-		
+
 	def print_info(self, print_C = False):
 		#print all relevant information to a problem instance
 		print('\nPROBLEM INSTANCE INFO:')
@@ -39,17 +39,17 @@ class Quadratic: #base class for all quadratic problem types
 		if print_C:
 			print('quadratic objective coeff vector C = \n' + str(self.C))
 		print("")
-			
+
 class Knapsack(Quadratic): #includes QKP, KQKP, QMKP
 	def __init__(self, seed=0, n=10, m=1, density=100, symmetric=True, k_item=False):
-		super().__init__(seed_=seed, n_=n, m_=m, density_=density, symmetric_=symmetric, k_item_=k_item) 
+		super().__init__(seed_=seed, n_=n, m_=m, density_=density, symmetric_=symmetric, k_item_=k_item)
 
 		#item weights
 		self.a = np.random.randint(low=1, high=51, size=(m, n))
 		#knapsack capacity and KQKP num_items constraints (multiple if QMKP)
 		if k_item:
 			for i in range(m):
-				self.num_items.append(np.random.randint(low=1, high=int(n/4)+1)) 
+				self.num_items.append(np.random.randint(low=1, high=int(n/4)+1))
 				self.b.append(np.random.randint(low=50, high=30*self.num_items[i])) #can get low>high errors
 		else:
 			for i in range(m):
@@ -66,10 +66,14 @@ class Knapsack(Quadratic): #includes QKP, KQKP, QMKP
 					if(symmetric):
 						self.C[i,j] = self.C[i,j]/2
 						self.C[j,i] = self.C[i,j]
-	
+
 class UQP(Quadratic): #unconstrained 0-1 quadratic program
+	"""
+	:objective: Linear and Quadratic values. Same objective as QKP. C[i,j] Can be negative!
+	:constraints: NO CONSTRAINTS (--> trivial if C[i,j] is positive for all i,j)
+	"""
 	def __init__(self, seed=0, n=10, m=1, density=100, symmetric=True):
-		super().__init__(seed_=seed, n_=n, m_=m, density_=density, symmetric_=symmetric) 
+		super().__init__(seed_=seed, n_=n, m_=m, density_=density, symmetric_=symmetric)
 		#item values
 		for i in range(n):
 			if(np.random.randint(1,101) <= density):
@@ -82,10 +86,14 @@ class UQP(Quadratic): #unconstrained 0-1 quadratic program
 					if(symmetric):
 						self.C[i,j] = self.C[i,j]/2
 						self.C[j,i] = self.C[i,j]
-	
+
 class HSP(Quadratic): #heaviest k-subgraph problem
+	"""
+	:objective: Only has quadratic values (no linear coefficients). C[i,j] = 0 or 1
+	:constraints: K-item constraint. Must take exactly k items
+	"""
 	def __init__(self, seed=0, n=16, density=50, symmetric=True, k_ratio=0.5):
-		super().__init__(seed_=seed, n_=n, density_=density, symmetric_=symmetric) 
+		super().__init__(seed_=seed, n_=n, density_=density, symmetric_=symmetric)
 		self.num_items = int(k_ratio*n)
 		#pair values
 		for i in range(n):
@@ -93,4 +101,4 @@ class HSP(Quadratic): #heaviest k-subgraph problem
 				if(np.random.randint(1,101) <= density):
 					self.C[i,j] = 1
 					if(symmetric):
-						self.C[j,i] = 1			
+						self.C[j,i] = 1
--- run_trials.py
+++ run_trials.py
@@ -2,36 +2,40 @@ from quadratics import *
 import cplex_solve as cplex
 import gurobi_solve as gurobi
 import sys
+import time
+import pandas as pd
 from timeit import default_timer as timer
 
 if(len(sys.argv)==5): #batch file will go through here
 	run_trials(trials=5,type=sys.argv[1],method=sys.argv[2],den=int(sys.argv[3]),size=int(sys.argv[4]))
 elif __name__=="__main__":
-	if False:
+	if True:
 		start = timer()
-
+		num_trials = 1
 		data = []
-		for i in range(1,3):
-			for j in range(3):
-				dict = cplex.run_trials(trials=2, type="QKP", method="std", size=10*i, den=100-(j*10))
+		for i in range(3,4): #i*10 gives sizes
+			for j in range(1): #100-(j*10) gives densities
+				dict = cplex.run_trials(trials=num_trials, type="QKP", method="std", size=10*i, den=100-(j*10))
 				data.append(dict)
-				dict = cplex.run_trials(trials=2, type="QKP", method="glover", size=10*i, den=100-(j*10))
+				dict = cplex.run_trials(trials=num_trials, type="QKP", method="glover", size=10*i, den=100-(j*10))
 				data.append(dict)
-				dict = cplex.run_trials(trials=2, type="QKP", method="glover_ext", size=10*i, den=100-(j*10))
+				dict = cplex.run_trials(trials=num_trials, type="QKP", method="glover_ext", size=10*i, den=100-(j*10))
 				data.append(dict)
-				dict = gurobi.run_trials(trials=2, type="QKP", method="std", size=10*i, den=100-(j*10))
-				data.append(dict)
-				dict = gurobi.run_trials(trials=2, type="QKP", method="glover", size=10*i, den=100-(j*10))
-				data.append(dict)
-				dict = gurobi.run_trials(trials=2, type="QKP", method="glover_ext", size=10*i, den=100-(j*10))
-				data.append(dict)
+				# dict = gurobi.run_trials(trials=num_trials, type="QKP", method="std", size=10*i, den=100-(j*10))
+				# data.append(dict)
+				# dict = gurobi.run_trials(trials=num_trials, type="QKP", method="glover", size=10*i, den=100-(j*10))
+				# data.append(dict)
+				# dict = gurobi.run_trials(trials=num_trials, type="QKP", method="glover_ext", size=10*i, den=100-(j*10))
+				# data.append(dict)
 				print("(i,j) = ("+str(i)+","+str(j)+")")
 
 		df = pd.DataFrame(data)
 		df = df[["solver", "type", "method", "size", "density", "avg_gap", "avg_solve_time", "std_dev"]]  #reorder columns
 		print(df)
-		#TODO put date in title so not overwritting
-		writer = pd.ExcelWriter('report.xlsx', engine='xlsxwriter')
+
+		time_stamp = time.strftime("%Y_%m_%d-%H_%M_%S")
+		excel_filename = "reports/"+time_stamp+'-report.xlsx'
+		writer = pd.ExcelWriter(excel_filename, engine='xlsxwriter')
 		df.to_excel(writer, index=False)
 		#df_footer.to_excel(writer, startrow=6, index=False) - to put multiple dataframes to same excel file
 		writer.save()
@@ -39,43 +43,44 @@ elif __name__=="__main__":
 		end = timer()
 		print("took " + str(end-start) + " seconds")
 
-knap = Knapsack()
-knap.print_info()
+if False:
+	knap = UQP()
+	knap.print_info(print_C=True)
 
-#CPLEX TESTS
-m = cplex.standard_linearization(knap)[0]
-m.solve()
-print(m.objective_value)
+	#CPLEX TESTS
+	m = cplex.standard_linearization(knap)[0]
+	m.solve()
+	print(m.objective_value)
 
-m = cplex.glovers_linearization(knap)[0]
-m.solve()
-print(m.objective_value)
+	m = cplex.glovers_linearization(knap)[0]
+	m.solve()
+	print(m.objective_value)
 
-m = cplex.reformulate_glover(knap)[0]
-m.solve()
-print(m.objective_value)
+	m = cplex.reformulate_glover(knap)[0]
+	m.solve()
+	print(m.objective_value)
 
-m = cplex.glovers_linearization_ext(knap)[0]
-m.solve()
-print(m.objective_value)
+	m = cplex.glovers_linearization_ext(knap)[0]
+	m.solve()
+	print(m.objective_value)
 
-#GUROBI TESTS
-m = gurobi.standard_linearization(knap)[0]
-m.setParam('OutputFlag',0)
-m.optimize();
-print(m.objVal)
+	#GUROBI TESTS
+	m = gurobi.standard_linearization(knap)[0]
+	m.setParam('OutputFlag',0)
+	m.optimize();
+	print(m.objVal)
 
-m = gurobi.glovers_linearization(knap)[0]
-m.setParam('OutputFlag',0)
-m.optimize()
-print(m.objVal)
+	m = gurobi.glovers_linearization(knap)[0]
+	m.setParam('OutputFlag',0)
+	m.optimize()
+	print(m.objVal)
 
-m = gurobi.reformulate_glover(knap)[0]
-m.setParam('OutputFlag',0)
-m.optimize()
-print(m.objVal)
+	m = gurobi.reformulate_glover(knap)[0]
+	m.setParam('OutputFlag',0)
+	m.optimize()
+	print(m.objVal)
 
-m = gurobi.glovers_linearization_ext(knap)[0]
-m.setParam('OutputFlag',0)
-m.optimize()
-print(m.objVal)
+	m = gurobi.glovers_linearization_ext(knap)[0]
+	m.setParam('OutputFlag',0)
+	m.optimize()
+	print(m.objVal)
