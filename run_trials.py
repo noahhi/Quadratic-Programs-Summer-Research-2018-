7a09a0c8d0e1247fedb5e37f9251a910f27ff74d
Hunt-Isaak <huntisan@dickinson.edu>
2018-06-20 17:10:17 -0400

    update run_trials to record average setup, solve and total times for set of trials

--- run_trials.py
+++ run_trials.py
@@ -13,9 +13,9 @@ def run_trials(trials=5,solver="cplex",type="QKP",symmetric=False,method="std",s
 	an average solve time
 	"""
 	#keep track of total run time across all trials to compute avg later
-	total_time, total_gap, total_obj = 0, 0, 0
+	setup_time_sum, solve_time_sum, int_gap_sum, obj_sum = 0,0,0,0
 	#need individual run time to compute standard deviation
-	run_times = []
+	instance_total_times = []
 
 	#write data to log file with descriptive name
 	description = solver+"-"+type+"-"+str(method)+"-"+str(size)+"-"+str(den)
@@ -63,9 +63,13 @@ def run_trials(trials=5,solver="cplex",type="QKP",symmetric=False,method="std",s
 						m = cplex.standard_linearization(quad, con1=False, con2=False)
 				elif method=="glover":
 					if options==0:
-						m = cplex.glovers_linearization(quad, use_diagonal=False)
+						m = cplex.glovers_linearization(quad, use_diagonal=False, lhs_constraints=True)
 					elif options==1:
-						m = cplex.glovers_linearization(quad, use_diagonal=True)
+						m = cplex.glovers_linearization(quad, use_diagonal=True, lhs_constraints=True)
+					elif options==2:
+						m = cplex.glovers_linearization(quad, use_diagonal=False, lhs_constraints=False)
+					elif options==3:
+						m = cplex.glovers_linearization(quad, use_diagonal=True, lhs_constraints=False)
 				elif method=="glover_rlt":
 					m = cplex.glovers_linearization_rlt(quad)
 				elif method=="glover_prlt":
@@ -96,87 +100,77 @@ def run_trials(trials=5,solver="cplex",type="QKP",symmetric=False,method="std",s
 			else:
 				raise Exception(str(solver) + "is not a valid solver type")
 
-			#retrieve setup time from modeling process and results from solve
-			setup_time = m[1]
-			solve_time = results.get("solve_time")
-			obj_val = results.get("objective_value")
-			int_gap = results.get("integrality_gap")
-			relax = results.get("relaxed_solution")
-			instance_time = setup_time+solve_time
-
-			#running totals across trials
-			run_times.append(instance_time)
-			total_time += instance_time
-			total_obj += obj_val
-			total_gap += int_gap
-
-			f.write("Integer Solution: " + str(obj_val)+"\n")
-			f.write("Continuous Solution: " + str(relax)+"\n")
-			f.write("Integrality Gap: " + str(int_gap)+"\n")
-			f.write("Setup Time: " + str(setup_time)+"\n")
-			f.write("Solve Time: " + str(solve_time)+"\n")
-			f.write("Instance Total Time (Setup+Solve): " + str(instance_time)+"\n")
+			#retrieve info from solving instance
+			instance_setup_time = m[1]
+			instance_solve_time = results.get("solve_time")
+			instace_obj_val = results.get("objective_value")
+			instace_int_gap = results.get("integrality_gap")
+			instance_relax = results.get("relaxed_solution")
+			instance_total_time = instance_setup_time + instance_solve_time
+			#print instance solve info to log file
+			f.write("Integer Solution: " + str(instace_obj_val)+"\n")
+			f.write("Continuous Solution: " + str(instance_relax)+"\n")
+			f.write("Integrality Gap: " + str(instace_int_gap)+"\n")
+			f.write("Setup Time: " + str(instance_setup_time)+"\n")
+			f.write("Solve Time: " + str(instance_solve_time)+"\n")
+			f.write("Instance Total Time (Setup+Solve): " + str(instance_total_time)+"\n")
 			f.write("=============================================\n")
 
-		results = {"solver":solver, "type":type, "method":method, "options":options, "size":size, "density":den, "avg_gap":total_gap/trials,
-					"avg_solve_time":total_time/trials, "std_dev":np.std(run_times), "avg_obj_val":total_obj/trials, "symmetric": symmetric}
+			#update running totals across trials for computing averages
+			instance_total_times.append(instance_total_time)
+			setup_time_sum += instance_setup_time
+			solve_time_sum += instance_solve_time
+			obj_sum += instace_obj_val
+			int_gap_sum += instace_int_gap
 
-		#print summary by iterating thru results dict
+		#return results across trials
+		results = {"solver":solver, "type":type, "method":method, "options":options, "size":size, "density":den, "avg_gap":int_gap_sum/trials,
+					"avg_total_time":(setup_time_sum+solve_time_sum)/trials, "std_dev":np.std(instance_total_times),
+					"avg_obj_val":obj_sum/trials, "symmetric": symmetric, "avg_setup_time": setup_time_sum/trials,
+					"avg_solve_time": solve_time_sum/trials}
+
+		#print results summary to log file by iterating through results dictionary
 		f.write("\n\nSummary Statistics\n")
 		f.write("=============================================\n")
-		f.write("Total solve time: " + str(total_time)+"\n")
-		f.write("Average Solve Time: " + str(total_time/trials)+"\n")
-		f.write("Solve Time Standard Deviation: " + str(np.std(run_times))+"\n")
-		f.write("Average Integrality Gap: " + str(total_gap/trials)+"\n")
+		for key,val in results.items():
+			f.write(key + " : " + str(val)+"\n")
 
+		#return results dictionary
 		return results
 
 if __name__=="__main__":
 	start = timer()
-	num_trials = 10
-	sizes = [50, 75, 100]
-	densities = [50, 100]
+	num_trials = 1
+	sizes = [15]
+	densities = [100]
+	solvers = ["cplex", "gurobi"]
 	data = []
 	for i in sizes:
 		for j in densities:
-			#TODO add a for solver in [cplex, gurobi]. for type in [QKP, ...] etc..
-			"""
-			solver = solver to use ("cplex", "gurobi")
-			type = problem type ("QKP", "KQKP", "UQP", "HSP")
-			method = linearization technique ("std", "glover", "glover_rlt", "glover_prlt")
-			options = specify alternative/optional constraints specific to each linearization
-			"""
-			print("current(size,density) = ("+str(i)+","+str(j)+")")
-			dict = run_trials(trials=num_trials, solver="cplex", type="QKP", symmetric=False, method="glover", size=i, den=j)
-			data.append(dict)
-			dict = run_trials(trials=num_trials, solver="cplex", type="QKP", symmetric=True, method="glover", size=i, den=j)
-			data.append(dict)
-			dict = run_trials(trials=num_trials, solver="cplex", type="QKP", symmetric=False, method="glover", size=i, den=j, options=1)
-			data.append(dict)
-			dict = run_trials(trials=num_trials, solver="cplex", type="QKP", symmetric=True, method="glover", size=i, den=j, options=1)
-			data.append(dict)
+			for solve_with in solvers:
+				#TODO add a for solver in [cplex, gurobi]. for type in [QKP, ...] etc..
+				"""
+				solver = solver to use ("cplex", "gurobi")
+				type = problem type ("QKP", "KQKP", "UQP", "HSP")
+				method = linearization technique ("std", "glover", "glover_rlt", "glover_prlt")
+				options = specify alternative/optional constraints specific to each linearization
+				"""
+				print("running with current(size,density,solver) = ("+str(i)+","+str(j)+","+solve_with+")...")
+				dict = run_trials(trials=num_trials, solver=solve_with, type="QKP", symmetric=False, method="std", size=i, den=j)
+				data.append(dict)
+				dict = run_trials(trials=num_trials, solver=solve_with, type="QKP", symmetric=True, method="std", size=i, den=j)
+				data.append(dict)
+				dict = run_trials(trials=num_trials, solver=solve_with, type="QKP", symmetric=False, method="std", size=i, den=j, options=1)
+				data.append(dict)
+				dict = run_trials(trials=num_trials, solver=solve_with, type="QKP", symmetric=True, method="std", size=i, den=j, options=1)
+				data.append(dict)
 
-			# dict = run_trials(trials=num_trials, solver="cplex", type="QKP", method="std", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="cplex", type="QKP", method="glover", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="cplex", type="QKP", method="glover_rlt", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="cplex", type="QKP", method="glover_prlt", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="gurobi", type="QKP", method="std", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="gurobi", type="QKP", method="glover", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="gurobi", type="QKP", method="glover_rlt", size=i, den=j)
-			# data.append(dict)
-			# dict = run_trials(trials=num_trials, solver="gurobi", type="QKP", method="glover_prlt", size=i, den=j)
-			# data.append(dict)
 
 	#TODO add to df more often/ not just once at the end
-	#TODO include setup time in here. more complete picture than just avg solve time 
+	#TODO use pickle to save the actual dataframe. Then can retrieve again and continue adding data to it
 	df = pd.DataFrame(data)
-	df = df[["solver", "type", "symmetric", "method","options", "size", "density", "avg_gap", "avg_solve_time", "std_dev", "avg_obj_val"]]  #reorder columns
+	df = df[["solver", "type", "symmetric", "method","options", "size", "density", "avg_gap",
+			"avg_setup_time", "avg_solve_time", "avg_total_time", "std_dev", "avg_obj_val"]]  #reorder columns
 	print(df)
 
 	time_stamp = time.strftime("%Y_%m_%d-%H_%M_%S")
